b6d5c61e8b34bb353f46b2947faa1dd64a36b282
Tyler <90929371+tyler-cog@users.noreply.github.com>
2023-12-05 12:20:49 -0600

    fixed issues and made better

--- Admin/EventHelper.php
+++ Admin/EventHelper.php
@@ -1,31 +1,28 @@
 <?php
     function validUIN($uin){
         include "../connection.php";
-        if ($uin == ""){
-            return true;
+    
+        // Prepare the SQL query using placeholders
+        $sql_query = "SELECT * FROM user WHERE UIN = ?";
+        $stmt = $db_conn->prepare($sql_query);
+    
+        if (!$stmt) {
+            die("Error preparing statement: " . $db_conn->error);
         }
-
-        $uin = (int)$uin;
-
-        if (($uin <= 0) || ($uin > 99999999999)){
-            return false;
+    
+        // Bind the parameter as a string
+        $stmt->bind_param("i", $uin);
+    
+        // Execute the query
+        if (!$stmt->execute()) {
+            die("Error executing query: " . $stmt->error);
         }
-
-        $sql_query = "SELECT * FROM `user` WHERE `UIN` = '$uin'";
-        $result = $db_conn->query($sql_query);
-
-        if (!$result) {
-            die("Query failed: " . $conn->error);
-        }
-        
-        // If there's a result
-        if ($result->num_rows > 0){
-            return false;
-        }
-
-        else {
-            return true;
-        }
+    
+        // Get the result
+        $result = $stmt->get_result();
+    
+        // Check if there are any rows (i.e., if the uin exists)
+        return $result->num_rows > 0;
     }
 
     function validEventID($event_id) {
@@ -54,53 +51,80 @@
         return $result->num_rows > 0;
     }
 
-    function INSERT_Event($event_id, $uin, $program_num, $start_date, $event_time, $location, $end_date, $event_type, $num_attend) {
+    function validProgramName($program_name) {
         include "../connection.php";
     
-        // SQL query to insert the event into the event table
-        $sql_query_insert_event = "INSERT INTO event 
-                                   (Event_ID, UIN, Program_Num, Start_Date, Time, Location, End_Date, Event_Type) 
-                                   VALUES 
-                                   ('$event_id', '$uin', '$program_num', '$start_date', '$event_time', '$location', '$end_date', '$event_type')";
+        // Prepare the SQL query using placeholders
+        $sql_query = "SELECT * FROM programs WHERE Name = ?";
+        $stmt = $db_conn->prepare($sql_query);
     
-        $result_insert_event = $db_conn->query($sql_query_insert_event);
+        if (!$stmt) {
+            die("Error preparing statement: " . $db_conn->error);
+        }
     
-        if (!$result_insert_event) {
-            die("Error inserting event: " . $db_conn->error);
+        // Bind the parameter as a string
+        $stmt->bind_param("s", $program_name);
+    
+        // Execute the query
+        if (!$stmt->execute()) {
+            die("Error executing query: " . $stmt->error);
+        }
+    
+        // Get the result
+        $result = $stmt->get_result();
+    
+        // Check if there are any rows (i.e., if the program name exists)
+        return $result->num_rows > 0;
+    }
+    
+    function INSERT_Event($event_id, $uin, $program_name, $start_date, $event_time, $location, $end_date, $event_type, $num_attend) {
+        include "../connection.php";
+        
+        // SQL query to select the Program_Num
+        $sql_num_query = "SELECT Program_Num FROM programs WHERE Name = '$program_name'";
+        
+        $result = $db_conn->query($sql_num_query);
+        
+        if ($result && $result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $program_num = $row['Program_Num'];
+    
+            // SQL query to insert the event into the event table
+            $sql_query_insert_event = "INSERT INTO event 
+                                       (Event_ID, UIN, Program_Num, Start_Date, Time, Location, End_Date, Event_Type) 
+                                       VALUES 
+                                       ('$event_id', '$uin', '$program_num', '$start_date', '$event_time', '$location', '$end_date', '$event_type')";
+    
+            $result_insert_event = $db_conn->query($sql_query_insert_event);
+    
+            if ($result_insert_event) {
+                // Get the last inserted ID if Event_ID is auto-incremented
+                $last_inserted_id = $db_conn->insert_id;
+                INSERT_tracking($uin, $num_attend, $last_inserted_id);
+            } else {
+                die("Error inserting event: " . $db_conn->error);
+            }
+        } else {
+            echo "No program found with the name: $program_name";
+        }
+    }
+    
+    function INSERT_tracking($uin, $num_attend, $event_id) {
+        include "../connection.php";
+        $sql_query_insert_update_attendance = "INSERT INTO event_tracking 
+            (Event_ID, UIN, ET_Num) 
+            VALUES 
+            ('$event_id', '$uin', '$num_attend')
+            ON DUPLICATE KEY UPDATE 
+            UIN = VALUES(UIN), ET_Num = VALUES(ET_Num)";
+
+        $result_insert_update_attendance = $db_conn->query($sql_query_insert_update_attendance);
+
+        if (!$result_insert_update_attendance) {
+            die("Error inserting or updating attendance information: " . $db_conn->error);
         }
     }
 
-    function INSERT_tracking($uin, $num_attend) {
-        include "../connection.php";
-    
-        // SQL query to search for the Event_ID associated with the given UIN
-        $sql_query_search_event_id = "SELECT Event_ID FROM event WHERE UIN = '$uin'";
-        $result_search_event_id = $db_conn->query($sql_query_search_event_id);
-    
-        if (!$result_search_event_id) {
-            die("Error searching for Event_ID: " . $db_conn->error);
-        }
-    
-        if ($result_search_event_id->num_rows > 0) {
-            $row = $result_search_event_id->fetch_assoc();
-            $event_id = $row['Event_ID'];
-    
-            // Now that you have the Event_ID, insert attendance information
-            $sql_query_insert_attendance = "INSERT INTO event_tracking 
-                (Event_ID, UIN, ET_Num) 
-                VALUES 
-                ('$event_id', '$uin', '$num_attend')";
-    
-            $result_insert_attendance = $db_conn->query($sql_query_insert_attendance);
-    
-            if (!$result_insert_attendance) {
-                die("Error inserting attendance information: " . $db_conn->error);
-            }
-        } else {
-            echo "No associated Event_ID found for UIN $uin";
-        }
-    }
-    
     function DELETE_Event($event_id) {
         include "../connection.php";
     
@@ -120,22 +144,25 @@
             die("Error deleting event: " . $db_conn->error);
         }
     }
-    
-    function DELETE_tracking($uin, $event_id) {
-        include "../connection.php";
-        // delete attendence portion  
-        $sql_query = "DELETE FROM event_tracking WHERE Event_ID = '$event_id' AND UIN = '$uin'";
 
-        $result = $db_conn->query($sql_query);
+    function UPDATE_Event($event_id, $uin, $program_name, $start_date, $event_time, $location, $end_date, $event_type, $num_attend) {
+        include "../connection.php"; // Adjust the path as necessary
+        
+        // SQL query to get the Program_Num
+        $sql_num_query = "SELECT Program_Num FROM programs WHERE Name = '$program_name'";
+        $result = $db_conn->query($sql_num_query);
 
-        if (!$result) {
-            die("Query failed: " . $conn->error);
+        // Initialize $program_num
+        $program_num = 0;
+
+        // Check if the query was successful and if it returned a row
+        if ($result && $result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $program_num = $row['Program_Num'];
+        } else {
+            die("Program not found: " . $db_conn->error);
         }
 
-    }
-    function UPDATE_Event($event_id, $uin, $program_num, $start_date, $event_time, $location, $end_date, $event_type) {
-        include "../connection.php"; // Adjust the path as necessary
-    
         // SQL query to update the event
         $sql_query = "UPDATE event SET 
                         UIN = ?, 
@@ -145,27 +172,28 @@
                         Location = ?, 
                         End_Date = ?, 
                         Event_Type = ? 
-                      WHERE Event_ID = ?";
+                    WHERE Event_ID = ?";
         
         // Prepare the statement
         $stmt = $db_conn->prepare($sql_query);
-    
+
         if (!$stmt) {
             die("Error preparing statement: " . $db_conn->error);
         }
-    
+
         // Bind the parameters to the placeholders
         $stmt->bind_param("sissssss", $uin, $program_num, $start_date, $event_time, $location, $end_date, $event_type, $event_id);
-    
+
         // Execute the statement
         if (!$stmt->execute()) {
             die("Error executing query: " . $stmt->error);
         }
-    
+        UPDATE_tracking($uin, $event_id, $num_attend);
+
         // Close the statement
         $stmt->close();
     }
-    
+  
     function UPDATE_tracking($uin, $event_id, $num_attend) {
         include "../connection.php";
         // Update attendance portion
@@ -192,14 +220,16 @@
 
     function GET_All_Events() {
         include "../connection.php"; // Adjust the path as necessary
-        
-        $sql_view = "CREATE OR REPLACE VIEW all_events_view AS
-            SELECT event.*, event_tracking.ET_Num
-            FROM event
-            LEFT JOIN event_tracking ON event.Event_ID = event_tracking.Event_ID";
 
+        $sql_view = "CREATE OR REPLACE VIEW all_events_view AS
+        SELECT event.*, event_tracking.ET_Num, programs.Name AS Name
+        FROM event
+        LEFT JOIN event_tracking ON event.Event_ID = event_tracking.Event_ID
+        LEFT JOIN programs ON event.Program_Num = programs.Program_Num";
+        
+        // $result = $db_conn->query($sql_view);
         if ($db_conn->query($sql_view) === TRUE) {
-                    
+            // echo $result;
         } else {
             echo "Error creating view: " . $db_conn->error;
         }
@@ -228,7 +258,7 @@
                     <tr>
                         <th>Event ID</th>
                         <th>UIN</th>
-                        <th>Program Number</th>
+                        <th>Program Name</th> <!-- Updated header -->
                         <th>Attendees</th>
                         <th>Start Date</th>
                         <th>Event Time</th>
@@ -236,26 +266,28 @@
                         <th>End Date</th>
                         <th>Event Type</th>
                     </tr>
-                </thead>
-                <tbody>';
+                  </thead>
+                  <tbody>';
     
         if ($events && $events->num_rows > 0) {
             while ($row = $events->fetch_assoc()) {
                 $html .= "<tr>
-                            <td>" . $row['Event_ID'] . "</td>
-                            <td>" . $row['UIN'] . "</td>
-                            <td>" . $row['Program_Num'] . "</td>
-                            <td>" . $row['ET_Num'] . "</td>
-                            <td>" . $row['Start_Date'] . "</td>
-                            <td>" . $row['Time'] . "</td>
-                            <td>" . $row['Location'] . "</td>
-                            <td>" . $row['End_Date'] . "</td>
-                            <td>" . $row['Event_Type'] . "</td>
+                            <td>" . htmlspecialchars($row['Event_ID']) . "</td>
+                            <td>" . htmlspecialchars($row['UIN']) . "</td>
+                            <td>" . htmlspecialchars($row['Name']) . "</td> <!-- Use the unique alias -->
+                            <td>" . htmlspecialchars($row['ET_Num']) . "</td>
+                            <td>" . htmlspecialchars($row['Start_Date']) . "</td>
+                            <td>" . htmlspecialchars($row['Time']) . "</td>
+                            <td>" . htmlspecialchars($row['Location']) . "</td>
+                            <td>" . htmlspecialchars($row['End_Date']) . "</td>
+                            <td>" . htmlspecialchars($row['Event_Type']) . "</td>
                           </tr>";
             }
         } else {
-            $html .= "<tr><td colspan='8'>No current events found.</td></tr>";
+            $html .= "<tr><td colspan='9'>No current events found.</td></tr>";
         }
+    
+        $html .= '</tbody></table></div>';
     
         return $html;
     }
