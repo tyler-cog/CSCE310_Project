42626d19b14591183191ce4527bc3fad40963c9b
gcarloin2002 <gcarloin2002@gmail.com>
2023-12-06 01:35:29 -0600

    added env file

---
+++ env.php
@@ -0,0 +1,8 @@
+<?php
+    // Defines variables needed to connnect to the database
+    $db_servername = "localhost"; 
+    $db_username = "root"; 
+    $db_password = ""; 
+    $db_database = "id21516113_cybersecurityclub_db";
+?>
+ 
